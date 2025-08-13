0660afe4ec779dcc33475f5b231e81852b41c01e
camilalazarozubillaga <furicafurica697@gmail.com>
2025-08-13 20:15:29 -0300

    CL - solucion-click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const div = document.getElementById("miDiv");
+    const btn = document.getElementById("btn");
+
     div.addEventListener("click", function() {
         alert("Hola! Soy el div");
+    });
+
+    btn.addEventListener("click", function(event) {
+        event.stopPropagation(); 
+        alert("Hola!");
     });
 });
